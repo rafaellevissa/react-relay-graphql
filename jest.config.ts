f84765c4fda286e8c97a6a1591aadc533055c8ea
Rafael Levi <rafaellevissa@gmail.com>
2024-06-14 13:50:33 -0300

    Fix jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,13 @@
 export default {
-    preset: 'ts-jest',
-    testEnvironment: 'jest-environment-jsdom',
-    rootDir: 'src',
-    transform: {
-        "^.+\\.tsx?$": "ts-jest" 
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  rootDir: "src",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
     // process `*.tsx` files with `ts-jest`
-    },
-    moduleNameMapper: {
-        '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/test/__ mocks __/fileMock.js',
-    },
-}
+  },
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
+  },
+};
